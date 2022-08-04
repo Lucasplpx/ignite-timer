8389ad32f0e570e2a740b6b489c10cf3d1b4800a
Lucas <lucasplpx@gmail.com>
2022-08-04 14:38:21 -0300

    melhorando inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,26 @@ export const Home = () => {
       <form action=''>
         <FormContainer>
           <label htmlFor='task'>Vou trabalhar em</label>
-          <TaskInput id='task' placeholder='Dê um nome para o seu projeto' />
+          <TaskInput
+            id='task'
+            list='task-suggestions'
+            placeholder='Dê um nome para o seu projeto'
+          />
+
+          <datalist id='task-suggestions'>
+            <option value='Projeto 1'></option>
+            <option value='Projeto 2'></option>
+            <option value='Projeto 3'></option>
+          </datalist>
 
           <label htmlFor='minutesAmount'>durante</label>
           <MinutesAmountInput
             id='minutesAmount'
             type='number'
             placeholder='00'
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
